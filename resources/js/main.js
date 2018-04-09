1f9bb8c4ca4ca1e107f2cf9f8eed5214af6c9a81
chasinglucid <35589621+chasinglucid@users.noreply.github.com>
2018-04-09 14:45:52 -0600

    Animate nav bar

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,3 +10,15 @@ $(document).ready(function () {
   });
 });
 
+var scrollTo = 0;
+$(window).scroll(function () {
+  var scrollTop = $(this).scrollTop();
+  if(scrollTop - scrollTo > 50) {
+    var navHeight = $('#nav').css('height');
+    $('#nav').animate({top: '-'+ navHeight}, 150);
+    scrollTo = scrollTop;
+  } else if (scrollTo - scrollTop > 50) {
+    $('#nav').animate({top: 0}, 150);  
+    scrollTo = scrollTop;
+  }
+});
